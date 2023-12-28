e4adc7efc15d2a03d0cec6eec698213a53f13eff
Bernardo Brandão <bbbrandao03@gmail.com>
2023-12-28 14:54:48 +0000

    First Commit

---
+++ airlines.h
@@ -0,0 +1,13 @@
+#ifndef PROJETO2AED_AIRLINES_H
+#define PROJETO2AED_AIRLINES_H
+#include <string>
+
+class Airlines {
+    private:
+        std::string Code;
+        std::string Name;
+        std::string Callsign;
+        std::string Country;
+};
+
+#endif //PROJETO2AED_AIRLINES_H
---
+++ flights.h
@@ -0,0 +1,13 @@
+#ifndef PROJETO2AED_FLIGHTS_H
+#define PROJETO2AED_FLIGHTS_H
+#include <string>
+
+class Flights {
+    private:
+        std::string Source;
+        std::string Target;
+        std::string Airline;
+
+};
+
+#endif //PROJETO2AED_FLIGHTS_H
