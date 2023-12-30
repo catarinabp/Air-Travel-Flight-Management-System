1639916820bd2437aaa268077b2ba2e2e1a9f20d
catarinabp <acatarinapatricio@gmail.com>
2023-12-30 18:12:58 +0000

    Menu & main updated - 1 to 10

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 #include "menu.h"
 #include "statistics.h"
 
+
 int main(int argc, char* argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    std::cout << "START TESTING" << std::endl << std::endl;
-    int res = RUN_ALL_TESTS();
+    //testing::InitGoogleTest(&argc, argv);
+    //std::cout << "START TESTING" << std::endl << std::endl;
+    //int res = RUN_ALL_TESTS();
 
-    return res;
-
-    //Call menu
+    //return res;
 
     int filter;
     int again = 1;
@@ -18,32 +17,207 @@ int main(int argc, char* argv[]) {
     while (again == 1) {
 
         std::cout << std::endl;
-
-        menu::Menu();
+        menu::Menu(); //Call menu
         std::cout << "Please choose the option you prefer by writing the number that corresponds to it and then pressing return: " << std::endl;
         std::cin >> filter;
 
-        while (filter < 0 || filter > 2) {
-            std::cout << std::endl;
-            std::cout << "Sorry, that is not a valid option." << std::endl;
+        //while (filter < 0 || filter > 10) {
+            //std::cout << std::endl;
+            //std::cout << "Sorry, that is not a valid option." << std::endl;
+            //std::cout << std::endl;
+            //std::cout << "Please choose the option you prefer by writing the number that corresponds to it and then pressing return: " << std::endl;
+            //std::cin >> filter;
+        //}
+
+        while (!(std::cin >> filter)) {
+            std::cout << "Invalid input. Please enter a valid number: ";
+            std::cin.clear();  // Clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Discard invalid input
             std::cout << std::endl;
         }
 
         //Global number of airports
         if (filter == 1) {
-            std::cout << "The global number of airports is " << numberOfAirports << std::endl;
-            std::cout << "\nType 1 to exit: ";
-            int exit;
-            std::cin >> exit;
+            std::cout << "The global number of airports is " << statistics::numberOfAirports() << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
         }
 
         //Global number of available flights
         if (filter == 2) {
-            std::cout << "The global number of available flights is " << numFlights << std::endl;
-            std::cout << "\nType 1 to exit: ";
-            int exit;
-            std::cin >> exit;
+            std::cout << "The global number of available flights is " << statistics::numFlights() << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
         }
+
+        //Number of flights out from an Airport
+        if (filter == 3) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The number of flights out from " << airportCode << "Airport is "
+                      << statistics::numberOfFlightsOut(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of different airlines with flights departing from an airport
+        if (filter == 4) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The number of different airlines with flights departing from " << airportCode << "airport is "
+                      << statistics::numberOfAirlinesOut(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of flights per airline
+        if (filter == 5) {
+            std::string airlineCode;
+            std::cout << "Please type the airline's code: ";
+            std::cin >> airlineCode;
+            std::cout << endl;
+            std::cout << "Number of flights of " << airlineCode << "airline is "
+                      << statistics::numberOfFlightsPerAirline(airlineCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of flights per city
+        if (filter == 6) {
+            std::string city;
+            std::cout << "Please type the city's name: ";
+            std::cin >> city;
+            std::cout << endl;
+            std::cout << "Number of flights from " << city << " is " << statistics::numberOfFlightsPerCity(city) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+
+        }
+
+        //List of destination airports reachable from an airport
+        if (filter == 7) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The list of destination airports reachable from " << airportCode
+                      << "airport is the following: " << std::endl;
+            std::cout << statistics::listOfDestinations(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of destination airports reachable from an airport
+        if (filter == 8) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The number of destination airports reachable from " << airportCode
+                      << "airport is: " << statistics::numberOfDestinationsAirports(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of cities reachable from an airport
+        if (filter == 9) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The number of cities reachable from " << airportCode
+                      << "airport is: " << statistics::numberOfDestinationsCities(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+        //Number of countries reachable from an airport
+        if (filter ==10) {
+            std::string airportCode;
+            std::cout << "Please type the airport's code: ";
+            std::cin >> airportCode;
+            std::cout << endl;
+            std::cout << "The number of countries reachable from " << airportCode
+                      << "airport is: " << statistics::numberOfDestinationsCountries(airportCode) << std::endl;
+            std::cout << "\nType 1 to return: ";
+            int userInput;
+            std::cin >> userInput;
+
+            // Validate user input if necessary
+            while (userInput != 1) {
+                std::cout << "Invalid input. Type 1 to return: ";
+                std::cin >> userInput;
+            }
+        }
+
+
 
 
         //END THE PROGRAM
--- menu.cpp
+++ menu.cpp
@@ -9,11 +9,19 @@ using namespace std;
 
 void menu::Menu() {
     cout << endl;
-    cout << "---------------------------------------------- Statistics ----------------------------------------------" << endl;
+    cout << "---------------------------------------------- Statistics of the network ----------------------------------------------" << endl;
     cout << endl;
     cout << "                 " << 1 << "Global number of airports" << endl;
     cout << "                 " << 2 << "Global number of available flights" << endl;
-
+    cout << "                 " << 3 << "Number of flights out from an airport" << endl;
+    cout << "                 " << 4 << "Number of different airlines with flights departing from an airport" << endl;
+    cout << "                 " << 5 << "Number of flights per airline;" << endl;
+    cout << "                 " << 6 << "Number of flights per city;" << endl;
+    cout << "                 " << 7 << "List of destination airports reachable from an airport;" << endl;
+    cout << "                 " << 8 << "Number of destination airports reachable from an airport;" << endl;
+    cout << "                 " << 9 << "Number of cities reachable from an airport;" << endl;
+    cout << "                " << 10 << "Number of countries reachable from an airport;" << endl;
     cout << endl;
-
+    cout << "                 " << "Press 0 to quit." << endl;
+    cout << endl;
 }
